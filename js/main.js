9ebd7bf9b21f2350c6d1776d5f2542340e6e1394
Alexandr Olimpiev <alexandr.olimpiev@gmail.com>
2020-02-03 22:11:27 +0200

    Выполнил задачу "Личный проект: больше деталей (часть 1)"

---
+++ js/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var AD_AUTHOR_AVATARS = ['01', '05', '08', '07', '02', '03', '06', '04'];
+var AD_OFFER_TITLES = ['Бронируйте', 'Только у нас', 'Дешево', 'Лучшее', 'Топ', 'Круто', 'The best', 'Срочно'];
+var AD_OFFER_ADDRESSES = ['100, 200', '500, 300', '150, 400', '400, 250', '100, 70', '360, 300', '420, 400', '190, 280', '330, 370'];
+var MIN_OFFER_PRICE = 200;
+var MAX_OFFER_PRICE = 2000;
+var AD_OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var MIN_OFFER_ROOMS = 1;
+var MAX_OFFER_ROOMS = 5;
+var MIN_OFFER_GUESTS = 1;
+var MAX_OFFER_GUESTS = 6;
+var AD_OFFER_TIMES = ['12:00', '13:00', '14:00'];
+var AD_OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var AD_OFFER_DESCRIPTION = 'Описание жилья';
+var AD_OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MIN_LOCATION_X = 0;
+var MIN_LOCATION_Y = 130;
+var MAX_LOCATION_Y = 630;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var ads = [];
+var map = document.querySelector('.map');
+var pinList = map.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('button');
+
+var getRandomValue = function (from, to) {
+  return Math.round(Math.random() * to) + from;
+};
+
+var getRandomElement = function (array) {
+  var i = getRandomValue(0, array.length - 1);
+  return array[i];
+};
+
+var makeAd = function name(i) {
+  var ad = {
+    'autor': {},
+    'offer': {},
+    'location': {}
+  };
+  ad.autor.avatar = 'img/avatars/user' + AD_AUTHOR_AVATARS[i] + '.png';
+  ad.offer.title = AD_OFFER_TITLES[i];
+  ad.offer.address = AD_OFFER_ADDRESSES[i];
+  ad.offer.price = getRandomValue(MIN_OFFER_PRICE, MAX_OFFER_PRICE);
+  ad.offer.type = getRandomElement(AD_OFFER_TYPES);
+  ad.offer.rooms = getRandomValue(MIN_OFFER_ROOMS, MAX_OFFER_ROOMS);
+  ad.offer.guests = getRandomValue(MIN_OFFER_GUESTS, MAX_OFFER_GUESTS);
+  ad.offer.checkin = getRandomElement(AD_OFFER_TIMES);
+  ad.offer.checkout = getRandomElement(AD_OFFER_TIMES);
+  ad.offer.features = getRandomElement(AD_OFFER_FEATURES);
+  ad.offer.description = AD_OFFER_DESCRIPTION;
+  ad.offer.photos = getRandomElement(AD_OFFER_PHOTOS);
+  ad.location.x = getRandomValue(MIN_LOCATION_X, map.clientWidth);
+  ad.location.y = getRandomValue(MIN_LOCATION_Y, MAX_LOCATION_Y);
+  return ad;
+};
+
+var getAds = function () {
+  var array = [];
+  for (var i = 0; i < 8; i++) {
+    array[i] = makeAd(i);
+  }
+  return array;
+};
+
+var renderPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = pin.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
+  var pinElementImage = pinElement.querySelector('img');
+  pinElementImage.src = pin.autor.avatar;
+  pinElementImage.alt = pin.offer.title;
+  return pinElement;
+};
+
+var renderRins = function (arrayPins) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 8; i++) {
+    fragment.appendChild(renderPin(arrayPins[i]));
+  }
+  return fragment;
+};
+
+ads = getAds();
+pinList.appendChild(renderRins(ads));
+map.classList.remove('map--faded');
